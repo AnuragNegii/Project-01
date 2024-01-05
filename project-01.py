36ebc5f62faf1fbf77140941947fb1fbfcd73d4c
abhay1223 <ayushnegi231@gmail.com>
2024-01-05 20:48:23 +0530

    added microphone funct.

--- project-01.py
+++ project-01.py
@@ -1,7 +1,37 @@
 import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+import speech_recognition as sr
+r = sr.Recognizer()
 
-engine.say("Anurag install pyAudio soon dude.... we  gotta start our project")
-engine.runAndWait()
+def SpeakText(command):
+  engine = pyttsx3.init()
+  voices = engine.getProperty('voices')
+  engine.setProperty('voice', voices[1].id)
+
+  engine.say(command)
+  engine.runAndWait()
+
+while(1):    
+     
+    
+    try:
+         
+     
+        with sr.Microphone() as source2:
+             
+          
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+          
+            audio2 = r.listen(source2)
+             
+            
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+ 
+            print(MyText)
+            SpeakText(MyText)
+             
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+         
+    except sr.UnknownValueError:
+        print("unknown error occurred")
